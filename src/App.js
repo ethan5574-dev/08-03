6d92a8bc2ba0f1fdd0bb0521b2bc38cf817d691f
Tranhau5574 <tranvanhaubg2003@gmail.com>
2023-02-20 16:22:08 +0700

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import WheelComponent from "./WheelComponent";
+export default function App() {
+  const segments = ["Happy", "Angry", "Sad", "Frustration", "Emptyness"];
+  const segColors = [
+    "#EE4040",
+    "#F0CF50",
+    "#815CD1",
+    "#3DA5E0",
+    // "#34A24F",
+    // "#F9AA1F",
+    // "#EC3F3F",
+    "#FF9000"
+  ];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
+  return (
+    <div className="App">
+      <div id="wheelCircle">
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          winningSegment=""
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          primaryColoraround="#ffffffb4"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={190}
+          upDuration={50}
+          downDuration={2000}
+        />
+      </div>
+    </div>
+  );
+}
